ecd065e8c695292ac0b64533d947a82ae4cd99ca
cameflow <alex.terrazas.z@gmail.com>
2016-09-27 12:30:31 -0500

    :bug: Fix screen size for menu

--- Assets/Scripts/lifeManager.cs
+++ Assets/Scripts/lifeManager.cs
@@ -2,33 +2,12 @@
 using System.Collections;
 
 public class lifeManager : MonoBehaviour {
-	private float nextActionTime = 0.0f;
-	public float period =1;
-	float posy = 485.0f;
-
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > nextActionTime) {
-			nextActionTime += period;
-			// execute block of code here
-			float size = transform.localScale.y;
-			if(size <= 0 )
-			{
-				print("Muerto");
-			}
-			else
-			{
-				float newsize = size - 0.001f;
-				transform.localScale = new Vector2 (1, newsize);
-				posy -= 0.285f;
-				transform.position = new Vector2 (91, posy);
-			}
-		}
 	}
 }
