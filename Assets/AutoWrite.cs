6ac9f9647a8df0c6eec749287c0884620e141021
Alexis <alexis.matuk@gmail.com>
2016-09-27 20:39:53 -0500

    Start terminal implementation

---
+++ Assets/AutoWrite.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoWrite : MonoBehaviour {
+    private string str;
+    void Start(){
+        StartCoroutine( AnimateText("Pretty cool text") );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator AnimateText(string strComplete){
+        int i = 0;
+        str = "";
+        while( i < strComplete.Length ){
+            str += strComplete[i++];
+            yield return new WaitForSeconds(0.5F);
+        }
+    }
+}
+
+/*
+Especificar string
+Especificar tiempo entre letras
+Especificar tiempo de espera antes
+Especificar tiempo de espear después
+
+*/
