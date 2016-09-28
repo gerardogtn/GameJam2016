a6f084ed33ce9bb725f961739fdc1e823c4a2001
Gerardo Teruel <gerardogtn@gmail.com>
2016-09-28 14:11:55 -0500

    Disable all colored blocks on start

--- Assets/Scripts/Blocks/ColoredBlock.cs
+++ Assets/Scripts/Blocks/ColoredBlock.cs
@@ -5,6 +5,11 @@ public class ColoredBlock : ColorReceiver {
 
 	public Colors color; 
 
+	public override void Start() {
+		base.Start ();
+		this.gameObject.SetActive (false);
+	}
+
 	public override void OnColorReceived(Colors color) {
 		Debug.Log (color.getString());
 		this.gameObject.SetActive (color == this.color);
