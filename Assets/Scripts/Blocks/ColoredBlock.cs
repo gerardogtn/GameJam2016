1c491a9591789b2ca17b51e01e19d4d010295b9e
Gerardo Teruel <gerardogtn@gmail.com>
2016-09-28 13:48:41 -0500

    Add logic to set colored blocks active

---
+++ Assets/Scripts/Blocks/ColoredBlock.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColoredBlock : ColorReceiver {
+
+	public Colors color; 
+
+	public override void OnColorReceived(Colors color) {
+		Debug.Log (color.getString());
+		this.gameObject.SetActive (color == this.color);
+	}
+}
