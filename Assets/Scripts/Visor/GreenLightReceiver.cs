ad2db72d6329afc03ff5409ba8d47e35ccb90285
Gerardo Teruel <gerardogtn@gmail.com>
2016-09-29 13:36:05 -0500

    Add visor scripts

---
+++ Assets/Scripts/Visor/GreenLightReceiver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GreenLightReceiver: ColorReceiver {
+
+	public Sprite EnabledSprite; 
+	public Sprite DisabledSprite;
+	private Image Image;
+
+	public override void Start () {
+		base.Start ();
+		Image = GetComponent<Image> ();
+		Image.sprite = DisabledSprite;
+	}
+
+	public override void OnColorReceived(Colors color) {
+		if (color == Colors.Blue || color == Colors.Yellow || color == Colors.Cyan || color == Colors.White) {
+			this.Image.sprite = EnabledSprite;
+		} else {
+			this.Image.sprite = DisabledSprite;
+		}
+	}
+}
---
+++ Assets/Scripts/Visor/RedLightReceiver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RedLightReceiver : ColorReceiver {
+
+	public Sprite EnabledSprite; 
+	public Sprite DisabledSprite;
+	private Image Image;
+
+	public override void Start () {
+		base.Start ();
+		Image = GetComponent<Image> ();
+		Image.sprite = DisabledSprite;
+	}
+
+	public override void OnColorReceived(Colors color) {
+		if (color == Colors.Red || color == Colors.Yellow || color == Colors.Magenta || color == Colors.White) {
+			this.Image.sprite = EnabledSprite;
+		} else {
+			this.Image.sprite = DisabledSprite;
+		}
+	}
+}
